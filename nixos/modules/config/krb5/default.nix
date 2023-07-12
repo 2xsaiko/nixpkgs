2a587755b4aa017c36c23361a12dbc33c043d634
Marco Rebhan <me@dblsaiko.net>
2023-07-13 01:21:59 +0200

    nixos/krb5: Cleanup, fix and RFC42-ify
    This replaces the krb5 module's options with RFC 42-style krb5.settings
    option*, while greatly simplifying the code and fixing a few bugs,
    namely:
    
    - #243068 krb5: Configuration silently gets ignored when set by
      multiple modules
    - not being able to use mkIf etc. inside subattributes of
      krb5.libdefaults, e.g. krb5.libdefaults.default_realm = mkIf ...
    
    * leaving an escape hatch in form of krb5.extraConfig in case extra
      syntax is needed (e.g. `include' directives)
    
    See #144575.
    Closes #243068.

--- nixos/modules/config/krb5/default.nix
+++ nixos/modules/config/krb5/default.nix
@@ -1,78 +1,76 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
+  inherit (lib) concatMapStringsSep concatStringsSep isAttrs isInt isList
+    isString literalExpression mapAttrsToList mdDoc mkEnableOption mkIf
+    mkOption mkRemovedOptionModule types;
+  inherit (lib.types) attrsOf bool either int lines listOf oneOf str;
+
+  prefixStringLines = prefix: str:
+    concatMapStringsSep "\n" (line: prefix + line) (lib.splitString "\n" str);
+  indent = prefixStringLines "  ";
+
+  toplevel = attrsOf section;
+  section = attrsOf relation;
+  relation = either (attrsOf value) value;
+  value = either (listOf atom) atom;
+  atom = oneOf [int str bool];
+
+  mkToplevel = toplevel: concatStringsSep "\n" (mapAttrsToList mkSection toplevel);
+  mkSection = name: section: ''
+    [${name}]
+    ${indent (concatStringsSep "\n" (mapAttrsToList mkRelation section))}
+  '';
+  mkRelation = name: relation:
+    if isAttrs relation
+    then ''
+      ${name} = {
+      ${indent (concatStringsSep "\n" (mapAttrsToList mkValue relation))}
+      }
+    ''
+    else mkValue name relation;
+  mkValue = name: value:
+    if isList value
+    then "${name} = ${concatMapStringsSep " " mkAtom value}"
+    else "${name} = ${mkAtom value}";
+  mkAtom = atom:
+    if atom == true
+    then "true"
+    else if atom == false
+    then "false"
+    else if isInt atom
+    then toString atom
+    else if isString atom
+    then atom
+    else throw "unreachable";
+
+  mkRemovedOptionModule' = name: reason: mkRemovedOptionModule ["krb5" name] reason;
+  mkRemovedOptionModuleCfg = name: mkRemovedOptionModule' name ''
+    The option `krb5.${name}' has been removed. Use `krb5.settings.${name}' for
+    structured configuration or `krb5.extraConfig' for plain-text configuration.
+  '';
+  mkRemovedOptionModuleVeryOld = name: replacement: mkRemovedOptionModule' name ''
+    The option `krb5.${name}' has been removed. Replace it with
+    `krb5.settings.${replacement}' according to krb5.conf(5).
+  '';
 
   cfg = config.krb5;
-
-  # This is to provide support for old configuration options (as much as is
-  # reasonable). This can be removed after 18.03 was released.
-  defaultConfig = {
-    libdefaults = optionalAttrs (cfg.defaultRealm != null)
-      { default_realm = cfg.defaultRealm; };
-
-    realms = optionalAttrs (lib.all (value: value != null) [
-      cfg.defaultRealm cfg.kdc cfg.kerberosAdminServer
-    ]) {
-      ${cfg.defaultRealm} = {
-        kdc = cfg.kdc;
-        admin_server = cfg.kerberosAdminServer;
-      };
-    };
-
-    domain_realm = optionalAttrs (lib.all (value: value != null) [
-      cfg.domainRealm cfg.defaultRealm
-    ]) {
-      ".${cfg.domainRealm}" = cfg.defaultRealm;
-      ${cfg.domainRealm} = cfg.defaultRealm;
-    };
-  };
-
-  mergedConfig = (recursiveUpdate defaultConfig {
-    inherit (config.krb5)
-      kerberos libdefaults realms domain_realm capaths appdefaults plugins
-      extraConfig config;
-  });
-
-  filterEmbeddedMetadata = value: if isAttrs value then
-    (filterAttrs
-      (attrName: attrValue: attrName != "_module" && attrValue != null)
-        value)
-    else value;
-
-  indent = "  ";
-
-  mkRelation = name: value:
-    if (isList value) then
-      concatMapStringsSep "\n" (mkRelation name) value
-    else "${name} = ${mkVal value}";
-
-  mkVal = value:
-    if (value == true) then "true"
-    else if (value == false) then "false"
-    else if (isInt value) then (toString value)
-    else if (isAttrs value) then
-      let configLines = concatLists
-        (map (splitString "\n")
-          (mapAttrsToList mkRelation value));
-      in
-      (concatStringsSep "\n${indent}"
-        ([ "{" ] ++ configLines))
-      + "\n}"
-    else value;
-
-  mkMappedAttrsOrString = value: concatMapStringsSep "\n"
-    (line: if builtins.stringLength line > 0
-      then "${indent}${line}"
-      else line)
-    (splitString "\n"
-      (if isAttrs value then
-        concatStringsSep "\n"
-            (mapAttrsToList mkRelation value)
-        else value));
-
 in {
+  imports = [
+    (mkRemovedOptionModuleCfg "libdefaults")
+    (mkRemovedOptionModuleCfg "realms")
+    (mkRemovedOptionModuleCfg "capaths")
+    (mkRemovedOptionModuleCfg "appdefaults")
+    (mkRemovedOptionModuleCfg "plugins")
+    (mkRemovedOptionModule' "config" ''
+      The option `krb5.config' has been removed. Use `krb5.settings' for
+      structured configuration or `krb5.extraConfig' for plain-text
+      configuration.
+    '')
+    (mkRemovedOptionModuleVeryOld "defaultRealm" "libdefaults.default_realm")
+    (mkRemovedOptionModuleVeryOld "domainRealm" "domain_realm")
+    (mkRemovedOptionModuleVeryOld "kdc" "realms.*.kdc")
+    (mkRemovedOptionModuleVeryOld "kerberosAdminServer" "realms.*.admin_server")
+  ];
 
   ###### interface
 
@@ -92,112 +90,66 @@ in {
         '';
       };
 
-      libdefaults = mkOption {
-        type = with types; either attrs lines;
+      settings = mkOption {
         default = {};
-        apply = attrs: filterEmbeddedMetadata attrs;
+        description = mdDoc ''
+          Structured contents of the `krb5.conf` file. See krb5.conf(5) for
+          details about configuration.
+        '';
         example = literalExpression ''
           {
-            default_realm = "ATHENA.MIT.EDU";
-          };
-        '';
-        description = lib.mdDoc ''
-          Settings used by the Kerberos V5 library.
-        '';
-      };
-
-      realms = mkOption {
-        type = with types; either attrs lines;
-        default = {};
-        example = literalExpression ''
-          {
-            "ATHENA.MIT.EDU" = {
-              admin_server = "athena.mit.edu";
-              kdc = [
-                "athena01.mit.edu"
-                "athena02.mit.edu"
-              ];
+            libdefaults = {
+              default_realm = "ATHENA.MIT.EDU";
             };
-          };
-        '';
-        apply = attrs: filterEmbeddedMetadata attrs;
-        description = lib.mdDoc "Realm-specific contact information and settings.";
-      };
 
-      domain_realm = mkOption {
-        type = with types; either attrs lines;
-        default = {};
-        example = literalExpression ''
-          {
-            "example.com" = "EXAMPLE.COM";
-            ".example.com" = "EXAMPLE.COM";
-          };
-        '';
-        apply = attrs: filterEmbeddedMetadata attrs;
-        description = lib.mdDoc ''
-          Map of server hostnames to Kerberos realms.
-        '';
-      };
+            realms = {
+              "ATHENA.MIT.EDU" = {
+                admin_server = "athena.mit.edu";
+                kdc = [
+                  "athena01.mit.edu"
+                  "athena02.mit.edu"
+                ];
+              };
+            };
 
-      capaths = mkOption {
-        type = with types; either attrs lines;
-        default = {};
-        example = literalExpression ''
-          {
-            "ATHENA.MIT.EDU" = {
-              "EXAMPLE.COM" = ".";
+            domain_realm = {
+              "example.com" = "EXAMPLE.COM";
+              ".example.com" = "EXAMPLE.COM";
             };
-            "EXAMPLE.COM" = {
-              "ATHENA.MIT.EDU" = ".";
-            };
-          };
-        '';
-        apply = attrs: filterEmbeddedMetadata attrs;
-        description = lib.mdDoc ''
-          Authentication paths for non-hierarchical cross-realm authentication.
-        '';
-      };
 
-      appdefaults = mkOption {
-        type = with types; either attrs lines;
-        default = {};
-        example = literalExpression ''
-          {
-            pam = {
-              debug = false;
-              ticket_lifetime = 36000;
-              renew_lifetime = 36000;
-              max_timeout = 30;
-              timeout_shift = 2;
-              initial_timeout = 1;
+            capaths = {
+              "ATHENA.MIT.EDU" = {
+                "EXAMPLE.COM" = ".";
+              };
+              "EXAMPLE.COM" = {
+                "ATHENA.MIT.EDU" = ".";
+              };
             };
-          };
-        '';
-        apply = attrs: filterEmbeddedMetadata attrs;
-        description = lib.mdDoc ''
-          Settings used by some Kerberos V5 applications.
-        '';
-      };
 
-      plugins = mkOption {
-        type = with types; either attrs lines;
-        default = {};
-        example = literalExpression ''
-          {
-            ccselect = {
-              disable = "k5identity";
+            appdefaults = {
+              pam = {
+                debug = false;
+                ticket_lifetime = 36000;
+                renew_lifetime = 36000;
+                max_timeout = 30;
+                timeout_shift = 2;
+                initial_timeout = 1;
+              };
             };
-          };
+
+            plugins = {
+              ccselect = {
+                disable = "k5identity";
+              };
+            };
+          }
         '';
-        apply = attrs: filterEmbeddedMetadata attrs;
-        description = lib.mdDoc ''
-          Controls plugin module registration.
-        '';
+        type = toplevel;
       };
 
       extraConfig = mkOption {
-        type = with types; nullOr lines;
-        default = null;
+        type = lines;
+        default = "";
         example = ''
           [logging]
             kdc          = SYSLOG:NOTICE
@@ -211,159 +163,17 @@ in {
           but it is not a recommended practice.
         '';
       };
-
-      config = mkOption {
-        type = with types; nullOr lines;
-        default = null;
-        example = ''
-          [libdefaults]
-            default_realm = EXAMPLE.COM
-
-          [realms]
-            EXAMPLE.COM = {
-              admin_server = kerberos.example.com
-              kdc = kerberos.example.com
-              default_principal_flags = +preauth
-            }
-
-          [domain_realm]
-            example.com  = EXAMPLE.COM
-            .example.com = EXAMPLE.COM
-
-          [logging]
-            kdc          = SYSLOG:NOTICE
-            admin_server = SYSLOG:NOTICE
-            default      = SYSLOG:NOTICE
-        '';
-        description = lib.mdDoc ''
-          Verbatim `krb5.conf` configuration.  Note that this
-          is mutually exclusive with configuration via
-          `libdefaults`, `realms`,
-          `domain_realm`, `capaths`,
-          `appdefaults`, `plugins` and
-          `extraConfig` configuration options.  Consult
-          `man krb5.conf` for documentation.
-        '';
-      };
-
-      defaultRealm = mkOption {
-        type = with types; nullOr str;
-        default = null;
-        example = "ATHENA.MIT.EDU";
-        description = lib.mdDoc ''
-          DEPRECATED, please use
-          `krb5.libdefaults.default_realm`.
-        '';
-      };
-
-      domainRealm = mkOption {
-        type = with types; nullOr str;
-        default = null;
-        example = "athena.mit.edu";
-        description = lib.mdDoc ''
-          DEPRECATED, please create a map of server hostnames to Kerberos realms
-          in `krb5.domain_realm`.
-        '';
-      };
-
-      kdc = mkOption {
-        type = with types; nullOr str;
-        default = null;
-        example = "kerberos.mit.edu";
-        description = lib.mdDoc ''
-          DEPRECATED, please pass a `kdc` attribute to a realm
-          in `krb5.realms`.
-        '';
-      };
-
-      kerberosAdminServer = mkOption {
-        type = with types; nullOr str;
-        default = null;
-        example = "kerberos.mit.edu";
-        description = lib.mdDoc ''
-          DEPRECATED, please pass an `admin_server` attribute
-          to a realm in `krb5.realms`.
-        '';
-      };
     };
   };
 
   ###### implementation
 
   config = mkIf cfg.enable {
+    environment = {
+      systemPackages = [cfg.kerberos];
+      etc."krb5.conf".text = cfg.extraConfig;
+    };
 
-    environment.systemPackages = [ cfg.kerberos ];
-
-    environment.etc."krb5.conf".text = if isString cfg.config
-      then cfg.config
-      else (''
-        [libdefaults]
-        ${mkMappedAttrsOrString mergedConfig.libdefaults}
-
-        [realms]
-        ${mkMappedAttrsOrString mergedConfig.realms}
-
-        [domain_realm]
-        ${mkMappedAttrsOrString mergedConfig.domain_realm}
-
-        [capaths]
-        ${mkMappedAttrsOrString mergedConfig.capaths}
-
-        [appdefaults]
-        ${mkMappedAttrsOrString mergedConfig.appdefaults}
-
-        [plugins]
-        ${mkMappedAttrsOrString mergedConfig.plugins}
-      '' + optionalString (mergedConfig.extraConfig != null)
-          ("\n" + mergedConfig.extraConfig));
-
-    warnings = flatten [
-      (optional (cfg.defaultRealm != null) ''
-        The option krb5.defaultRealm is deprecated, please use
-        krb5.libdefaults.default_realm.
-      '')
-      (optional (cfg.domainRealm != null) ''
-        The option krb5.domainRealm is deprecated, please use krb5.domain_realm.
-      '')
-      (optional (cfg.kdc != null) ''
-        The option krb5.kdc is deprecated, please pass a kdc attribute to a
-        realm in krb5.realms.
-      '')
-      (optional (cfg.kerberosAdminServer != null) ''
-        The option krb5.kerberosAdminServer is deprecated, please pass an
-        admin_server attribute to a realm in krb5.realms.
-      '')
-    ];
-
-    assertions = [
-      { assertion = !((builtins.any (value: value != null) [
-            cfg.defaultRealm cfg.domainRealm cfg.kdc cfg.kerberosAdminServer
-          ]) && ((builtins.any (value: value != {}) [
-              cfg.libdefaults cfg.realms cfg.domain_realm cfg.capaths
-              cfg.appdefaults cfg.plugins
-            ]) || (builtins.any (value: value != null) [
-              cfg.config cfg.extraConfig
-            ])));
-        message = ''
-          Configuration of krb5.conf by deprecated options is mutually exclusive
-          with configuration by section.  Please migrate your config using the
-          attributes suggested in the warnings.
-        '';
-      }
-      { assertion = !(cfg.config != null
-          && ((builtins.any (value: value != {}) [
-              cfg.libdefaults cfg.realms cfg.domain_realm cfg.capaths
-              cfg.appdefaults cfg.plugins
-            ]) || (builtins.any (value: value != null) [
-              cfg.extraConfig cfg.defaultRealm cfg.domainRealm cfg.kdc
-              cfg.kerberosAdminServer
-            ])));
-        message = ''
-          Configuration of krb5.conf using krb.config is mutually exclusive with
-          configuration by section.  If you want to mix the two, you can pass
-          lines to any configuration section or lines to krb5.extraConfig.
-        '';
-      }
-    ];
+    krb5.extraConfig = mkToplevel cfg.settings;
   };
 }
